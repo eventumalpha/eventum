366b0390e4c4d1ee0bf3d77e3be356a085d6c317
eventumalpha <hello@medrm.online>
2025-05-02 10:21:59 +0200

    Update script.js.js

--- script.js.js
+++ script.js.js
@@ -1,1 +1,14 @@
-console.log("Welcome to Eventum Shield Playground");
+window.addEventListener('load', () => {
+  document.querySelector('.preloader').style.display = 'none';
+});
+
+const toggle = document.getElementById("soundToggle");
+const audio = document.getElementById("ambient");
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+});
